bddf9ad8ed547f23686c7b686799b0168b823c16
Bryyyan93 <brayanpalacios93@gmail.com>
2024-10-11 19:37:23 +0200

    Intentar que se conecte

--- modules/security_group/main.tf
+++ modules/security_group/main.tf
@@ -1,22 +1,24 @@
 resource "aws_security_group" "this" {
   name        = var.sg_name
-  description = "Allow HTTP y SSH traffic to ALB"
+  description = "Allow HTTP traffic to ALB"
   vpc_id = var.vpc_id
 
+ # Permitir tráfico HTTP (puerto 80) desde cualquier origen
   ingress {
     from_port   = 80
     to_port     = 80
     protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]  # Permite tráfico HTTP desde cualquier IP
   }
-
+/*
     ingress {
     from_port   = 22
     to_port     = 22
     protocol    = "tcp"
     cidr_blocks = [var.local_cidr]  # Permitir tráfico SSH solo desde la red local
   }
-
+*/
+  # Permitir tráfico de salida (egress) a cualquier destino
   egress {
     from_port   = 0
     to_port     = 0
--- modules/vpc/main.tf
+++ modules/vpc/main.tf
@@ -1,39 +1,12 @@
 resource "aws_vpc" "this" {
   cidr_block           = var.vpc_cidr
-  enable_dns_support   = true
-  enable_dns_hostnames = false
+  #enable_dns_support   = true
+  enable_dns_hostnames = true
 
   tags = {
     Name = var.vpc_name
   }
 }
-/*
-resource "aws_subnet" "private_subnet" {
-  for_each = toset(var.availability_zones)  # Crea una subnet por cada zona de disponibilidad
-  vpc_id                  = aws_vpc.this.id
-  cidr_block              = cidrsubnet(var.vpc_cidr, 8, index(tolist(var.availability_zones), each.key))  # Usa un índice numérico basado en la lista de availability_zones
-  map_public_ip_on_launch = true # Esto asigna IPs públicas automáticamente
-  availability_zone       =  each.value #var.availability_zones # Usa each.value para la zona de disponibilidad
-
-  tags = {
-    Name = "${var.subnet_name}-${each.value}"
-  }
-}
-
-resource "aws_route_table" "private_route_table" {
-  vpc_id = aws_vpc.this.id
-
-  tags = {
-    Name = var.route_table_name
-  }
-}
-
-resource "aws_route_table_association" "private_association" {
-  for_each = aws_subnet.private_subnet # Necesitamos for_each aquí para recorrer las subnets
-  subnet_id      = each.value.id # Accede a cada subnet usando each.value.id
-  route_table_id = aws_route_table.private_route_table.id
-}
-*/
 # crear subnets publicas
 resource "aws_subnet" "public_subnet" {
   for_each = toset(var.availability_zones) # Una subnet pública por cada zona de disponibilidad
